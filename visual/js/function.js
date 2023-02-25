f70f133e4c25ae5aaee5b3e795e1f5b36a1cf0f9
Axeel19 <126359190+Axeel19@users.noreply.github.com>
2023-02-25 18:45:22 -0300

    Add files via upload
    Curriculm

---
+++ visual/js/function.js
@@ -0,0 +1,28 @@
+  document.getElementById('texto1').onclick= function () {
+    document.getElementById('texto').style.display= 'block'
+  }
+
+  document.getElementById('texto2').onclick= function () {
+    document.getElementById('texto').style.display= 'none'
+  }
+
+  document.getElementById('boton').onclick= function () {
+    document.getElementById('parrafo').style.display= 'block'
+  }
+
+  document.getElementById('boton2').onclick= function () {
+    document.getElementById('parrafo').style.display= 'none'
+  }
+
+  document.getElementById('boton1').onclick= function () {
+    document.getElementById('parrafo1').style.display= 'block'
+  }
+  
+  document.getElementById('boton3').onclick= function () {
+  document.getElementById('parrafo1').style.display= 'none'
+  }
+    
+  
+    
+
+        
